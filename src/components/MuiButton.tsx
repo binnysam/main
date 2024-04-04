1157338692c587c5be644a59d511fb34d7fb4283
binnysam <binnysamuelchristy@gmail.com>
2024-04-04 08:20:27 +0300

    added button components

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+export const MuiButton = () => {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Button
+      </Button>
+    </div>
+  );
+};
